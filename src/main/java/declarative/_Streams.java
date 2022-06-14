a1dabb52d58a1be7c1767518c245e27ddb013da9
Cody McCarty <codyjmccarty@gmail.com>
2022-06-14 00:05:19 -0400

    basic streams

--- src/main/java/declarative/_Streams.java
+++ src/main/java/declarative/_Streams.java
@@ -5,6 +5,9 @@ import datainfo.DataInfo.Gender;
 import datainfo.DataInfo.Person;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class _Streams {
@@ -16,6 +19,17 @@ public class _Streams {
     Set<Gender> genders = people.stream()
         .map(person -> person.getGender())
         .collect(Collectors.toSet());
+
+    // promoted to var
+    Function<Person, String> personStringFunction = person -> person.getName();
+    Function<String, Integer> stringIntegerFunction = name -> name.length();
+    Collector<Integer, ?, List<Integer>> integerListCollector = Collectors.toList();
+    Predicate<Integer> integerPredicate = length -> length > 0;
+    List<Integer> nameLengths = people.stream()
+        .map(personStringFunction)
+        .map(stringIntegerFunction)
+        .filter(integerPredicate)
+        .collect(integerListCollector);
   }
 
 }
